27c35db71cccb997f2ab933284b2d1052a6b3900
Nicola Benaglia <nico.benaz@gmail.com>
2023-09-08 00:12:06 +0200

    Finish vecs

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -9,7 +9,9 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40];     // a plain array
-    let v = vec![10, 20, 30, 40]; // TODO: declare your vector here with the macro for vectors
+    // TODO: declare your vector here with the macro for vectors
+    let v = a.to_vec();
+    // let v = vec![10, 20, 30, 40]; 
 
     (a, v)
 }
