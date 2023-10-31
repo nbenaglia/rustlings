0d2c08af6e683cb74921e19842973ed688e42eb3
Nicola Benaglia <nico.benaz@gmail.com>
2023-10-31 15:12:56 +0100

    Clippy

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -8,9 +8,7 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        my_option.unwrap();
-    }
+    if let Some(x) = my_option {};
 
     let my_arr = &[
         -1, -2, -3
@@ -18,7 +16,8 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    let my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
